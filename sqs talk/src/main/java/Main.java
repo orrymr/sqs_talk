55d9c68dad23234dd8c03892c2619447aa2f821c
orrym <orrym@vatit.com>
2018-02-09 15:26:02 +0200

    create main class, and override

--- sqs talk/src/main/java/Main.java
+++ sqs talk/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("test");
+	public static void main(String[] args) throws IOException {
+		// Create your own override.properties from the override.properties.template file
+		// Put the path to you override.properties file here
+		String propertiesPath = "/home/orrymr/Documents/sqs talk/sqs talk/src/main/resources/override.properties";
+
+		Properties appProps = new Properties();
+		appProps.load(new FileInputStream(propertiesPath));
+		System.out.println(appProps.getProperty("testProp"));
+
+		AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
+
 	}
 }
